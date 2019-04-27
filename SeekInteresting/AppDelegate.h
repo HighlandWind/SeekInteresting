e795c89216bb628b1c29601e7cbfad8aeb880c29
LiuGJ <arlenliugj@sina.cn>
2019-04-27 12:54:54 +0800

    Initial Commit

---
+++ SeekInteresting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SeekInteresting
+//
+//  Created by lgj on 2019/4/27.
+//  Copyright © 2019年 LiuGJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
